2d57946d5b23a8b197e8994abc4accf8628d50f4
Celvin Braun <46591058+CelvinBraun@users.noreply.github.com>
2021-12-15 18:07:42 +0100

    example added

---
+++ hello_world.ino/hello_world.ino.ino
@@ -0,0 +1,10 @@
+#include "DigiKeyboardDe.h"  
+void setup() {  
+  DigiKeyboardDe.sendKeyStroke(0);  
+  DigiKeyboardDe.sendKeyStroke(KEY_R, MOD_GUI_LEFT);   
+  DigiKeyboardDe.println("cmd");
+  DigiKeyboard.delay(500); 
+  DigiKeyboardDe.println("echo Hello World!");
+}  
+void loop() {  
+} 
